1ce246baad5ff50975e72873b23549cbbd95f482
SirSerje <sir.serje@gmail.com>
2019-03-21 13:34:58 +0200

    initial commit

---
+++ app/index.js
@@ -0,0 +1,13 @@
+/**
+ * Application entry point
+ */
+
+// Load application styles
+import 'styles/index.scss';
+import A from 'tag';
+
+console.log('!!!!', A);
+document.getElementsByTagName('body')[0].appendChild(document.createElement("B").appendChild( document.createTextNode("Water")));
+// ================================
+// START YOUR APP HERE
+// ================================
